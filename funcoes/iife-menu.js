6ab80e0a207db58c190792568a0f44424a97ae82
Luiz Saurin <luizsaurin21@gmail.com>
2022-08-02 18:43:28 -0300

    update

---
+++ funcoes/iife-menu.js
@@ -0,0 +1,11 @@
+(function(win, doc){
+    let isValid = true
+    console.log("menu isValid ", isValid)
+
+    function init() {
+        console.log("init do menu")
+    }
+    init()
+
+    win.alert("ola mundo")
+})(window, document)
